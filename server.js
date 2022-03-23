d4b53a69c7508cb983f67521aa16fb4ae0e09fe6
Hector Nava <hector.r.nava44@gmail.com>
2022-03-23 09:09:43 -0600

    Added routes folder for APIs

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
+const express = require('express');
 const mongoose = require('mongoose');
-const User = require('./model/User');
-const Todo = require('./model/Todo');
+const routes = require('./routes');
+
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
 
 mongoose.connect('mongodb://localhost:27017/todoMongoDB')
     .then(async () => {
         console.log('Successfully connected to MongoDB');
-        const newUser = await User.create({
-            username: 'XRP FTW',
-            role: 'Employee',
-            powerLevel: 9001
-        });
-
-        const newTodo = await Todo.create({
-            text: 'Go for a run'
-        });
-
-        console.log(newTodo);
     })
     .catch(error => console.log(error));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+app.listen(PORT, () => console.log('Server is running'))
